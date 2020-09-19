78d85fc6c6478d9aefe13a5a09a8517c9be136b5
Rahul Gopinath <rahul@gopinath.org>
2020-09-19 13:11:32 -0700

    .

---
+++ examples/hello2decoder.py
@@ -0,0 +1,19 @@
+import stateless.generate as G
+from stateless.status import *
+
+def validate(inputstr):
+    try:
+        if inputstr[0] != b'h':
+            return Status.Incorrect, None, None
+        if len(inputstr) < 5:
+            return Status.Incomplete, None, None
+        if inputstr[1:3] != b'el':
+            return Status.Incorrect, 1, None
+        if inputstr[3] != b'l':
+            return Status.Incorrect, None, None
+        if inputstr[4] != b'l':
+            return Status.Incorrect, None, None
+        return Status.Complete, None, None
+    except G.NeedMoreException as e:
+        return Status.Incomplete, None, None
+
