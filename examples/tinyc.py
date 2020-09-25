d074ff70d45028e26f03bdc672fe4a840ae2b241
Rahul Gopinath <rahul@gopinath.org>
2020-09-25 02:55:53 -0700

    update

--- examples/tinyc.py
+++ examples/tinyc.py
@@ -1,4 +1,5 @@
 import subprocess
+import tempfile
 import stateless.generate as G
 from stateless.status import *
 
@@ -9,13 +10,14 @@ def validate(input_str):
         c: the character where error happened  "" if not applicable
     """
     try:
-        with open('input.tmp', 'wb+') as f:
+        with tempfile.NamedTemporaryFile() as f:
             f.write(input_str.b)
-        cmd = "./examples/tiny/tiny"
-        p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        stdout,stderr = p.communicate()
-        output = stderr
-        output = output.decode("utf-8")
+            f.flush()
+            cmd = "./examples/tiny/tiny %s" % f.name
+            p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            stdout,stderr = p.communicate()
+            output = stderr
+            output = output.decode("utf-8")
         if output.startswith("syntax"):
             return Status.Incorrect, None, None
         elif output.startswith("EOF"):
