467ef4803bdb8c5fd211ac2091072f6ef6e92071
Rahul Gopinath <rahul@gopinath.org>
2020-09-25 05:45:07 -0700

    tinyc

--- examples/tinyc.py
+++ examples/tinyc.py
@@ -1,4 +1,4 @@
-import subprocess
+import os
 import tempfile
 import stateless.generate as G
 from stateless.status import *
@@ -9,29 +9,16 @@ def validate(input_str):
         n: the index of the character -1 if not applicable
         c: the character where error happened  "" if not applicable
     """
-    try:
-        with tempfile.NamedTemporaryFile() as f:
-            f.write(input_str.b)
-            f.flush()
-            cmd = "./examples/tiny/tiny %s" % f.name
-            p = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-            stdout,stderr = p.communicate()
-            output = stderr
-            output = output.decode("utf-8")
-        if output.startswith("syntax"):
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(input_str.b)
+        f.flush()
+        cmd = "./examples/tiny/tiny %s" % f.name
+        res = os.system(cmd)
+        res = os.WEXITSTATUS(res)
+        res = (256-res) * (-1) if res > 127 else res # short conversion
+        if res == 1:
             return Status.Incorrect, None, None
-        elif output.startswith("EOF"):
+        elif res == -1:
             return Status.Incomplete, None, None
-        elif output.find("Syntax error") != -1:
-            return Status.Incorrect, None, None
         else:
             return Status.Complete, None, None
-    except Exception as e:
-        msg = str(e)
-        if msg.endswith("exit status 1"):
-            pass
-        else:
-            pass
-        n = len(msg)
-        return Status.Incorrect, None, None
-
