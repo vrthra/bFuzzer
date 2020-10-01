a6b6f64f80c06b428efc5f90c207e0ea9c8c6cc5
Rahul Gopinath <rahul@gopinath.org>
2020-10-01 05:41:27 -0700

    .

--- examples/hello2decoder.py
+++ examples/hello2decoder.py
@@ -1,19 +1,64 @@
 import stateless.generate as G
+import stateless.exceptions as E
 from stateless.status import *
+from stateless.utils import *
+import random
+random.seed(0)
 
-def validate(inputstr):
-    try:
-        if inputstr[0] != b'h':
-            return Status.Incorrect, None, None
-        if len(inputstr) < 3:
-            return Status.Incomplete, None, None
-        if inputstr[1:3] != b'el':
-            return Status.Incorrect, 1, None
-        if inputstr[3] != b'l':
-            return Status.Incorrect, 3, None
-        if inputstr[4] != b'o':
-            return Status.Incorrect, 4, None
-        return Status.Complete, None, None
-    except G.NeedMoreException as e:
-        return Status.Incomplete, None, None
+class MyBytearray:
+    def __init__(self, int_arr):
+        self.b = bytearray(int_arr)
+
+    def __len__(self):
+        return len(self.b)
+
+    def __eq__(self, o):
+        if isinstance(o, MyBytearray):
+            return self.b == o.b
+        return self.b == o
+
+    def __getitem__(self, idx):
+        if isinstance(idx, int):
+            if len(self.b) <= idx:
+                raise E.NeedMoreException()
+            return bytes([self.b[idx]])
+        elif isinstance(idx, slice):
+            if idx.start >= len(self.b):
+                raise E.NeedMoreException()
+            if idx.stop is not None and idx.stop > len(self.b):
+                raise E.NeedMoreException()
+            return MyBytearray(self.b[idx])
+        else:
+            assert False, idx
+
+    def __repr__(self):
+        return 'MyBytearray[%s]' % repr(self.b)
+
+
+class Hello2Validate(Validate):
+    def __init__(self, exe):
+        self.exe = exe
+
+    def validate(self, inputstr_):
+        inputstr = MyBytearray(inputstr_)
+        try:
+            if inputstr[0] != b'h':
+                return Status.Incorrect, None
+            if len(inputstr) < 3:
+                return Status.Incomplete, None
+            if inputstr[1:3] != b'el':
+                return Status.Incorrect, 1
+            if inputstr[3] != b'l':
+                return Status.Incorrect, 3
+            if inputstr[4] != b'o':
+                return Status.Incorrect, 4
+            return Status.Complete, None
+        except G.NeedMoreException as e:
+            return Status.Incomplete, None
+
+
+    def get_cumulative_coverage(self, input_str):
+       ...
+
+validator = Hello2Validate('')
 
--- examples/hellodecoder.py
+++ examples/hellodecoder.py
@@ -1,17 +1,60 @@
 import stateless.generate as G
+import stateless.exceptions as E
 from stateless.status import *
+from stateless.utils import *
+import random
+random.seed(0)
 
-def validate(inputstr):
-    try:
-        if inputstr[0] != b'H':
-            return Status.Incorrect, None, None
-        if inputstr[1] != b'E':
-            return Status.Incorrect, None, None
-        if inputstr[2] != b'L':
-            return Status.Incorrect, None, None
-        if inputstr[3:5].b != b'LO':
-            return Status.Incorrect, None, None
-        return Status.Complete, None, None
-    except G.NeedMoreException as e:
-        return Status.Incomplete, None, None
+class MyBytearray:
+    def __init__(self, int_arr):
+        self.b = bytearray(int_arr)
 
+    def __len__(self):
+        return len(self.b)
+
+    def __eq__(self, o):
+        if isinstance(o, MyBytearray):
+            return self.b == o.b
+        return self.b == o
+
+    def __getitem__(self, idx):
+        if isinstance(idx, int):
+            if len(self.b) <= idx:
+                raise E.NeedMoreException()
+            return bytes([self.b[idx]])
+        elif isinstance(idx, slice):
+            if idx.start >= len(self.b):
+                raise E.NeedMoreException()
+            if idx.stop is not None and idx.stop > len(self.b):
+                raise E.NeedMoreException()
+            return MyBytearray(self.b[idx])
+        else:
+            assert False, idx
+
+    def __repr__(self):
+        return 'MyBytearray[%s]' % repr(self.b)
+
+
+class HelloValidate(Validate):
+    def __init__(self, exe):
+        self.exe = exe
+
+    def validate(self, inputstr_):
+        inputstr = MyBytearray(inputstr_)
+        try:
+            if inputstr[0] != b'H':
+                return Status.Incorrect, None
+            if inputstr[1] != b'E':
+                return Status.Incorrect, None
+            if inputstr[2] != b'L':
+                return Status.Incorrect, None
+            if inputstr[3:5].b != b'LO':
+                return Status.Incorrect, None
+            return Status.Complete, None
+        except E.NeedMoreException as e:
+            return Status.Incomplete, None
+
+    def get_cumulative_coverage(self, input_str):
+       ...
+
+validator = HelloValidate('')
