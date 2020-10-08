0b1360f8e978c271dab9b98f0e72ee9b66e7db5f
Rahul Gopinath <rahul@gopinath.org>
2020-10-08 04:01:23 -0700

    .

--- examples/mjs/mjs.py
+++ examples/mjs/mjs.py
@@ -5,10 +5,13 @@ class MjsValidate(Validate):
         self.exe = exe
 
     def _cov(self, res):
-        o = res.stdout.decode().split('\n')
-        l = o[6].replace('Lines executed:', '').split(' ')[0][:-1]
-        b = o[8].replace('Taken at least once:', '').split(' ')[0][:-1]
-        return (l, b)
+        try:
+            o = res.stdout.decode().split('\n')
+            l = o[6].replace('Lines executed:', '').split(' ')[0][:-1]
+            b = o[8].replace('Taken at least once:', '').split(' ')[0][:-1]
+            return (l, b)
+        except IndexError as e:
+            return (-1, -1)
 
 validator = MjsValidate('./examples/mjs/mjs')
 
