7e0c1d4e7aacd6ea26900d219b3260f23a2fc027
Rahul Gopinath <rahul@gopinath.org>
2020-10-13 23:06:06 -0700

    .

--- check_inputs.py
+++ check_inputs.py
@@ -29,7 +29,33 @@ class PFuzzerValidator(Validate):
                 cov_result = self._cov(res2)
                 return cov_result
 
+class PFuzzerTinycValidator(PFuzzerValidator):
+    #def f_exec(self, exe, fname):
+    #    return do([exe, '-f', fname])
+
+    def _cov(self, res):
+        s = res.stdout.decode().split('\n')
+        assert not s[5]
+        assert s[6] == "File 'tiny.c'"
+        l = s[7].replace('Lines executed:', '').split(' ')[0][:-1]
+        b = s[9].replace('Taken at least once:', '').split(' ')[0][:-1]
+        return (l, b)
+
+class PFuzzerCjsonValidator(PFuzzerValidator):
+    #def f_exec(self, exe, fname):
+    #    return do([exe, '-f', fname])
+
+    def _cov(self, res):
+        s = res.stdout.decode().split('\n')
+        assert not s[5]
+        assert s[6] == "File 'cJSON.c'"
+        l = s[7].replace('Lines executed:', '').split(' ')[0][:-1]
+        b = s[9].replace('Taken at least once:', '').split(' ')[0][:-1]
+        return (l, b)
+
 class PFuzzerMjsValidator(PFuzzerValidator):
+    #def f_exec(self, exe, fname):
+    #    return do([exe, '-f', fname])
 
     def _cov(self, res):
         s = res.stdout.decode().split('\n')
@@ -40,19 +66,27 @@ class PFuzzerMjsValidator(PFuzzerValidator):
 
 def check_valid_inputs(exe, my_data, name):
     lst_generated = []
-    pf = (PFuzzerMjsValidator(exe) if name == 'mjs' else PFuzzerValidator(exe))
+    if name == 'mjs':
+        pf = PFuzzerMjsValidator(exe)
+    elif name == 'tiny':
+        pf = PFuzzerTinycValidator(exe)
+    elif name == 'cjson':
+        pf = PFuzzerCjsonValidator(exe)
+    else:
+        pf = PFuzzerValidator(exe)
 
     with open('examples/results_%s.json' % name, 'w+') as f:
         for i,(t,b) in enumerate(my_data):
-            try:
+                # try:
+                pf.idx = i
                 c = pf.get_cumulative_coverage(b)
                 lst_generated.append((i,c))
                 print(json.dumps({'output':[j for j in b], 
                                   'cumcoverage': c,
                                   'time': t}), 
                       file=f, flush=True)
-            except Exception as e:
-                print(i, e)
+                # except Exception as e:
+                # print(i, e)
     return lst_generated
 
 
--- interp_results.py
+++ interp_results.py
@@ -4,6 +4,7 @@ import sys
 with open(sys.argv[1]) as f:
     lines = f.readlines()
     for l in lines:
+        if l[0] == '#': continue
         s = json.loads(l)
         print(bytes(s['output']))
 
